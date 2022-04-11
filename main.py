718516353277bddf3db0133b522205dae4d0f0bc
Robert Walery <robert.walery.99@gmail.com>
2022-04-11 21:40:12 +0200

    Amplitude spectrum

--- main.py
+++ main.py
@@ -44,8 +44,30 @@ def DFT():
     # planes[0] = Re, planes[1] = Im
     cv.magnitude(planes[0], planes[1], amplitude_spectrum)
 
-    # cv.imshow("spectrum magnitude", amplitude_spectrum)
-    # cv.waitKey()
+    # there shouldn't be zero in any cell before logarithm
+    ones_matrix = np.ones(amplitude_spectrum.shape, np.float32)
+    cv.add(ones_matrix, amplitude_spectrum, amplitude_spectrum)
+
+    cv.log(amplitude_spectrum, amplitude_spectrum)
+
+    # move quarters of the picture - coordinates of the input image(0,0) will be in the center of spectrum
+    center_x = int(amplitude_spectrum.shape[0] / 2)
+    center_y = int(amplitude_spectrum.shape[1] / 2)
+    top_left = amplitude_spectrum[0:center_x, 0:center_y]
+    top_right = amplitude_spectrum[center_x:center_x + center_x, 0:center_y]
+    bottom_left = amplitude_spectrum[0:center_x, center_y:center_y + center_y]
+    bottom_right = amplitude_spectrum[center_x: center_x + center_x, center_y: center_y + center_y]
+    tmp = np.copy(top_left)
+    amplitude_spectrum[0:center_x, 0:center_y] = bottom_right
+    amplitude_spectrum[center_x: center_x + center_x, center_y: center_y + center_y] = tmp
+    tmp = np.copy(top_right)
+    amplitude_spectrum[center_x:center_x + center_x, 0:center_y] = bottom_left
+    amplitude_spectrum[0:center_x, center_y: center_y + center_y] = tmp
+
+    cv.normalize (amplitude_spectrum, amplitude_spectrum, 0, 1, cv.NORM_MINMAX)
+
+    cv.imshow("spectrum magnitude", amplitude_spectrum)
+    cv.waitKey()
 
 
 display_image()
