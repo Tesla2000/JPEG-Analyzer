a899a1122793abb5abaee676e1350337c88fa269
bsierp <sith20001@wp.pl>
2022-06-21 17:06:58 +0200

    CBC mode - not working

--- classes/rsa.py
+++ classes/rsa.py
@@ -15,8 +15,8 @@ class RSA:
         self.n = self.p * self.q
         self.phi = (self.p - 1) * (self.q - 1)
         while not euclidean(self.e, self.phi) == 1:
-            self.e = random.randint(1, self.phi)
-        self.d = pow(self.e, -1, self.phi)
+            self.e = random.randint(1, self.phi)  # public key
+        self.d = pow(self.e, -1, self.phi)  # private key
 
     def encrypt(self, m):
         return pow(m, self.e, self.n)
@@ -39,3 +39,31 @@ class RSA:
         list_decrypted += \
             self.decrypt(int.from_bytes(list_split[-1], byteorder='big')).to_bytes(last_len, byteorder='big')
         return list_decrypted
+
+    def list_encrypt_cbc(self, li, block_size, init_vec=0):
+        list_split, last_length = split(li, block_size)
+        list_encrypted = [
+            self.encrypt(int.from_bytes(list_split[0], byteorder='big') ^ init_vec).to_bytes(32, byteorder='big')]
+        for i in range(1, len(list_split)):
+            list_encrypted.append(self.encrypt(int.from_bytes(list_split[i], byteorder='big') ^ int.from_bytes(
+                list_encrypted[i-1], byteorder='big')).to_bytes(32, byteorder='big'))
+        list_encrypted = b''.join(list_encrypted)
+        return list_encrypted, last_length
+
+    def list_decrypt_cbc(self, li, block_size, last_len, init_vec=0):
+        list_split, last_length = split(li, 32)
+        list_decrypted = [
+            (self.decrypt(int.from_bytes(list_split[0], byteorder='big')) ^
+             init_vec).to_bytes(block_size, byteorder='big')]
+        for i in range(1, len(list_split) - 1):
+            list_decrypted.append((self.decrypt(int.from_bytes(
+                list_split[i], byteorder='big') ^ int.from_bytes(list_split[i-1], byteorder='big'))).to_bytes(block_size, byteorder='big'))
+        list_decrypted.append((self.decrypt(int.from_bytes(
+            list_split[-1], byteorder='big') ^ int.from_bytes(list_split[-2], byteorder='big'))).to_bytes(last_len, byteorder='big'))
+        list_decrypted = b''.join(list_decrypted)
+        return list_decrypted
+
+    def print_keys(self):
+        print("Public key: ", self.e)
+        print("Private key: ", self.d)
+        print("N:", self.n)
--- run_encrypter.py
+++ run_encrypter.py
@@ -3,13 +3,13 @@ from classes.rsa import RSA
 
 # rsa = RSA()
 # c = [0x12, 0x13, 0x14, 0x15, 0x12, 0x13, 0x14, 0x15, 0x12, 0x13, 0x14, 0x15, 0x12, 0x13, 0x14, 0x15, 0x12, 0x13, 0x14, 0x15, 0x12, 0x13, 0x14, 0x15, ]
-# test = rsa.list_encrypt(c, 3)
+# test = rsa.list_encrypt_cbc(c, 3)
 # print(test)
 # print(len(test[0]))
-# test2 = rsa.list_decrypt(test[0], 3, test[1])
+# test2 = rsa.list_decrypt_cbc(test[0], 3, test[1])
 # print(test2)
 
-jpeg = JpegImage('JPEG_JFIF.jpg', 60)
+jpeg = JpegImage('JPEG_JFIF.jpg', 16)
 jpeg.read_markers()
 jpeg.save_encrypted()
 jpeg.save_decrypted()
